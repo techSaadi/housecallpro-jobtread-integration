b15a9d3e72fc528a24a8cf00df5c097aec9ab173
https://github.com/techSaadi <techsavvy.saadi@gmail.com>
2025-03-10 21:58:07 +0500

    update requirements.txt

--- app.py
+++ app.py
@@ -101,4 +101,5 @@ def home():
 
 # Run the Flask app
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT variable
+    app.run(host="0.0.0.0", port=port)
